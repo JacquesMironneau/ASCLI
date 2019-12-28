f2132e2ff56361cd2908ebe44dc8d8e9bdaef1d8
Pashmiii <lowneonema@gmail.com>
2019-12-28 22:34:10 +0100

    Add the lil old controller again

---
+++ src/system/ASCController.java
@@ -0,0 +1,26 @@
+package system;
+
+public interface ASCController {
+
+	//Play a playlist or a track
+	public void play();
+	
+	//...
+	public void pause();
+	
+	public void skip();
+	
+	//Just display the API output
+	public void search();
+	
+	//=> display what is currently played
+	public void nowPlaying();
+	
+	
+	public void addToQueue();
+	
+	
+	
+	
+	
+}
