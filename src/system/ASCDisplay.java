0c8169b5b9605f50aff7272bc7139d6ed9d5f556
Pashmiii <lowneonema@gmail.com>
2019-12-28 20:30:48 +0100

    First commit, add MVC pattern

---
+++ src/system/ASCDisplay.java
@@ -0,0 +1,19 @@
+package system;
+
+public interface ASCDisplay {
+
+	
+	//Prolly with an Error Enum (no track played/Nothing found/Server error...)
+	public void displayError();
+	
+	
+	//Same shit as above 
+	public void displaySuccess();
+	
+	
+	//Display what is currently play
+	public void displayTrack();
+	
+	//Display next maybe
+	public void displayNext();
+}
---
+++ src/system/Launcher.java
@@ -0,0 +1,9 @@
+package system;
+
+public class Launcher {
+
+	public static void main (String[] args)
+	{
+		System.out.println("I got jdk");
+	}
+}
