705e570d7e217287bfb79aa10a651d63d43e6333
Wyrellis <wyrellis>
2019-12-28 21:48:04 +0100

    create a class ASCControllerConcret

---
+++ src/system/ASCControllerConcret.java
@@ -0,0 +1,38 @@
+package system;
+
+public class ASCControllerConcret implements ASCController {
+
+	//Attributes
+	private ASCDisplay myDisplay;
+
+	//methods
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void skip() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void search() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void nowPlaying() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void addToQueue() {
+		// TODO Auto-generated method stub
+	}
+}
