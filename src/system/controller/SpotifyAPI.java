b01c23fd79c51ad3f1ed10262be602f9fca56bae
Wyrellis <wyrellis>
2019-12-29 17:39:33 +0100

    add constants

--- src/system/controller/SpotifyAPI.java
+++ src/system/controller/SpotifyAPI.java
@@ -1,9 +1,17 @@
 package system.controller;
 
+//import com.google.gson.JsonElement;
+
 import java.net.URI;
+import java.text.ParseException;
+import java.util.logging.Logger;
 
 public class SpotifyAPI {
 
+	  public static final String AUTHENTIFICATION = "accounts.spotify.com";
+	  public static final String BASE_URL = "https://api.spotify.com/v1/track/id";
+	  public static final int PORT_HTTPS = 443;
+	  
 }
 
 // https://developer.spotify.com/console/get-track/?id=3n3Ppam7vgaVa1iaRUc9Lp
