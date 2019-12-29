6b86f9b6b0c3c2ea1b90d42430830effdbedc39d
Pashmiii <lowneonema@gmail.com>
2019-12-29 14:19:00 +0100

    Arrange package in a cleaner way

--- src/system/ASCInputReader.java
+++
@@ -1,7 +0,0 @@
-package system;
-
-public interface ASCInputReader 
-{
-
-	public String read();
-}
--- src/system/ASCStringRetreiver.java
+++
@@ -1,7 +0,0 @@
-package system;
-
-//Create a command from a readed string
-public interface ASCStringRetreiver {
-
-	public ASCCommand retreive(String toRetreive) throws ErrorInUserInput;
-}
