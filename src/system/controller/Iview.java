e3d4acace9ad1b994d75dc8c40747c9812a1ae06
Wyrellis <wyrellis>
2020-02-12 21:52:22 +0100

    add constants

---
+++ src/system/controller/Iview.java
@@ -0,0 +1,14 @@
+package system.controller;
+
+public interface Iview {
+
+	 static final int OK = 200;
+	 static final int CREATED = 201;
+	 static final int ACCEPTED = 202;
+	 static final int NO_CONTENT = 204;
+	 static final int BAD_REQUEST = 400;
+	 static final int FORBIDDEN = 403;
+	 static final int TOO_MANY_REQUESTS = 429;
+
+	
+}
